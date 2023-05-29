efe2999c8c6fc8923a7a16ea27cb55e99cd7151c
fahmiyonda007 <yondadf04@gmail.com>
2023-05-29 15:05:52 +0700

    [UPD] brand

--- resources/views/vendor/filament/components/brand.blade.php
+++ resources/views/vendor/filament/components/brand.blade.php
@@ -3,6 +3,10 @@
         'filament-brand text-xl font-bold tracking-tight',
         'dark:text-white' => config('filament.dark_mode'),
     ])>
+        {{-- <img src="{{ asset('/logo-inline.png') }}" alt="Icon"
+            class="relative z-20 block h-10 object-contain dark:hidden" />
+        <img src="{{ asset('/logo-inline-dark.png') }}" alt="Icon"
+            class="relative z-20 hidden h-10 object-contain dark:block" /> --}}
         {{ $brand }}
     </div>
 @endif
